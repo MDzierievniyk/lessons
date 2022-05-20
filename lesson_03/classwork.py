f856db3b8ff12bc85ca49bbe133e29327e9af808
MDzierievniyk <derevnukm@gmail.com>
2022-05-20 21:31:57 +0300

    Add my classwork

---
+++ lesson_03/classwork.py
@@ -0,0 +1,52 @@
+
+In [3]: x = 17 / 2 * 3 + 2
+
+In [4]: x
+Out[4]: 27.5
+
+In [5]: x = 17 / 2 * (3 + 2)
+
+In [6]: x
+Out[6]: 42.5
+
+In [7]: x = 2 + 17 / 2 * 3
+
+In [8]: x
+Out[8]: 27.5
+
+In [11]: x = 2 + 17 /( 2 * 3)
+
+In [12]: x
+Out[12]: 4.833333333333334
+
+In [13]: x = 2 + 17 / 2 * 3
+
+In [16]: sanya,kirill,yarik=18,16,17
+
+In [17]: sanya+kirill+yarik
+Out[17]: 51
+
+In [18]: avg=(sanya+kirill+yarik)/3
+
+In [19]: avg
+Out[19]: 17.0
+
+In [20]: my_list = [1, 1.0, 2, 2, 5.0, "python", "python3", "python3"]
+
+In [21]: set(my_list)
+Out[21]: {1, 2, 5.0, 'python', 'python3'}
+
+
+In [23]: myset=set(my_list)
+
+In [24]: len(my_list)-len(myset)
+Out[24]: 3
+
+In [25]: my_list[2:5]
+Out[25]: [2, 2, 5.0]
+
+In [33]: size=6
+
+In [34]: [size*4,size**2,size*size,(size**2*2)**0.5]
+Out[34]: [24, 36, 36, 8.48528137423857]
+
