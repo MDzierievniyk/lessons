67194863b89c50a5bed30a62b601efd9505c9f67
MDzierievniyk <derevnukm@gmail.com>
2022-05-25 01:04:41 +0300

    Add my homework files

---
+++ lesson_03/solution_02.py
@@ -0,0 +1,18 @@
+In [1]: dev = 2130
+
+In [2]: bonus=120
+
+In [3]: percent = 0.1
+
+In [4]: first_year=dev+dev*percent+bonus
+
+In [5]: second_year=first_year+first_year*percent+bonus
+
+In [6]: third_year=second_year+second_year*percent+bonus
+
+In [7]: fourth_year=third_year+third_year*percent+bonus
+
+In [8]: fifth_year=fourth_year+fourth_year*percent+bonus
+
+In [9]: print(fifth_year)
+4162.9983
---
+++ lesson_03/solution_03.py
@@ -0,0 +1,7 @@
+[1]: hw3_list=[1,"a","b","c",7,4]
+
+In [2]: hw3_string=map(str,hw3_list)
+
+In [3]: print(" ".join(hw3_string))
+1 a b c 7 4
+
