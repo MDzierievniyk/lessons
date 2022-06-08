eca74c7410bba28173d9717030254ba807425b58
MDzierievniyk <derevnukm@gmail.com>
2022-06-08 19:20:27 +0300

    Add my homework

---
+++ lesson_07/solution_01.py
@@ -0,0 +1,17 @@
+def is_figures_beat_each_other(x1, y1, x2, y2):
+    return abs(abs (x1-x2)-abs(y1-y2))==1
+
+
+def main():
+    x1 = int(input("Enter x1: "))
+    x2 = int(input("Enter x2: "))
+    y1 = int(input("Enter y1: "))
+    y2 = int(input("Enter y2: "))
+    if is_figures_beat_each_other(x1, y1, x2, y2):
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    main()
